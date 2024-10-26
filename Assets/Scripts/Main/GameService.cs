d94f34e499139e31366ee1adb9263f8efd96f04f
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-10-26 20:26:32 +0530

    Use Service Locator

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,5 +1,10 @@
+using ServiceLocator.Events;
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +12,31 @@ using UnityEngine;
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService { get; private set; }
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    public SoundService soundService { get; private set; }
+    public WaveService waveService { get; private set; }
+    public MapService mapService { get; private set; }
+    public EventService eventService { get; private set; }
 
+    [SerializeField] private UIService uiService;
+    public UIService UIService=> uiService;
+
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
+
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+    [SerializeField] private MapScriptableObject mapScriptableObject;
     void Start()
     {
+        eventService = new EventService();
+        UIService.SubscribeToEvents();
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject,audioEffects,backgroundMusic);
+        waveService = new WaveService(waveScriptableObject);
+        mapService = new MapService(mapScriptableObject);
     }
 
     // Update is called once per frame
